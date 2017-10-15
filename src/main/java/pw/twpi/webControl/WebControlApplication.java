453e153775f2cbb836b5003053a50a5b844cd610
PotatoSauceVFX <rj@potatosaucevfx.com>
2017-10-15 12:34:51 -0400

    Initial Commit

---
+++ src/main/java/pw/twpi/webControl/WebControlApplication.java
@@ -0,0 +1,12 @@
+package pw.twpi.webControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebControlApplication.class, args);
+	}
+}
